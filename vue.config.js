53017fe27fe6eb4019ba6dce3c03158577b2b78d
Douglas Wade <douglas.b.wade@gmail.com>
2022-04-12 23:29:56 -0700

    add publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: { port: 8000 },
+  publicPath: "/dougwade.io/",
 
   pluginOptions: {
     vuetify: {
