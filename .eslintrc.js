51f736397f4069ff25fe06a971ff4237e94f9a16
Douglas Wade <douglas.b.wade@gmail.com>
2022-04-12 23:30:41 -0700

    Deploy

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: { node: true, jest: true },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
-  rules: {
-    "vue/match-component-file-name": ["error"],
-  },
-};
---
+++ js/about.3a37dd22.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdougwade_io"]=self["webpackChunkdougwade_io"]||[]).push([[443],{7574:(a,e,u)=>{u.r(e),u.d(e,{default:()=>l});var s=u(3396),n={class:"about"},d=(0,s._)("h1",null,"This is an about page",-1),i=[d];function o(a,e){return(0,s.wg)(),(0,s.iD)("div",n,i)}var r=u(89);const t={},c=(0,r.Z)(t,[["render",o]]),l=c}}]);
+//# sourceMappingURL=about.3a37dd22.js.map
